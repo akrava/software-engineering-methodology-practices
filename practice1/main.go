e30cd2d7a2e2f9045cec84e23eefc92f7b707b06
Arkadiy Krava <arkadiy@ex.ua>
2022-05-05 00:54:36 +0300

    Verify all contries are neighborhoods without overlap

--- practice1/main.go
+++ practice1/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/akrava/software-engineering-methodology-practices/practice1/common"
 	"github.com/akrava/software-engineering-methodology-practices/practice1/eurodiffusion"
 	"github.com/akrava/software-engineering-methodology-practices/practice1/parser"
 )
@@ -14,9 +15,17 @@ func main() {
 		log.Printf("Couldn't parse test cases from input: %v", err)
 		os.Exit(1)
 	}
+	testCasesResults := []common.TestCaseResults{}
 	for i, testCase := range testCases {
+		if err := common.VerifyAllCountriesAreNeighborhoodsAndNonOverlapping(testCase); err != nil {
+			log.Printf("Test case #%d is invalid: %v", i, err)
+			os.Exit(1)
+		}
 		results := eurodiffusion.CalculateEuroDiffusionForTestCase(testCase)
-		if err := parser.WriteResultsOfTestCase(os.Stdout, results, i+1); err != nil {
+		testCasesResults = append(testCasesResults, results)
+	}
+	for i, testCaseResult := range testCasesResults {
+		if err := parser.WriteResultsOfTestCase(os.Stdout, testCaseResult, i+1); err != nil {
 			log.Printf("Couldn't write results of test case #%d: %v", i+1, err)
 			os.Exit(1)
 		}
